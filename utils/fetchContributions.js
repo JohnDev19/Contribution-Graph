148338703ceeaf2c7481fe90922638e5cd0de891
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-22 16:42:49 +0800

    Update fetchContributions.js

--- utils/fetchContributions.js
+++ utils/fetchContributions.js
@@ -1,71 +1,63 @@
+import axios from 'axios';
+
+export async function fetchContributions(username, token) {
+  const query = `
+    query($username: String!) {
+      user(login: $username) {
+        contributionsCollection {
+          contributionCalendar {
+            totalContributions
+            weeks {
+              contributionDays {
+                contributionCount
+                date
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const variables = { username };
+
+  try {
+    const response = await axios.post('https://api.github.com/graphql', {
+      query,
+      variables
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    const contributions = response.data.data.user.contributionsCollection.contributionCalendar;
+    return generateSVG(contributions);
+  } catch (error) {
+    console.error('Error fetching contributions:', error);
+    throw error;
+  }
+}
+
 function generateSVG(contributions) {
   const width = 720;
   const height = 112;
   const cellSize = 10;
   const cellPadding = 2;
 
-  let svg = `
-  <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
+  let svg = `<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
     <style>
       .contribution { fill: #ebedf0; }
       .contribution-1 { fill: #9be9a8; }
       .contribution-2 { fill: #40c463; }
       .contribution-3 { fill: #30a14e; }
       .contribution-4 { fill: #216e39; }
-      text { font: 9px Arial; fill: #767676; }
-    </style>
-    <g transform="translate(20, 5)">
-      ${generateMonths(contributions)}
-      ${generateDays()}
-      ${generateCells(contributions, cellSize, cellPadding)}
-    </g>
-    <g transform="translate(20, ${height - 10})">
-      <text>Less</text>
-      <rect x="35" y="-9" width="10" height="10" class="contribution"/>
-      <rect x="50" y="-9" width="10" height="10" class="contribution-1"/>
-      <rect x="65" y="-9" width="10" height="10" class="contribution-2"/>
-      <rect x="80" y="-9" width="10" height="10" class="contribution-3"/>
-      <rect x="95" y="-9" width="10" height="10" class="contribution-4"/>
-      <text x="110" y="0">More</text>
-    </g>
-  </svg>`;
+    </style>`;
 
-  return {
-    svg,
-    totalContributions: contributions.totalContributions
-  };
-}
-
-function generateMonths(contributions) {
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  let monthsSvg = '';
-  let currentMonth = '';
   let x = 0;
-
-  contributions.weeks.forEach((week, i) => {
-    const date = new Date(week.contributionDays[0].date);
-    const month = months[date.getMonth()];
-    if (month !== currentMonth) {
-      monthsSvg += `<text x="${x}" y="-7">${month}</text>`;
-      currentMonth = month;
-    }
-    x += 14;
-  });
-
-  return monthsSvg;
-}
-
-function generateDays() {
-  const days = ['Sun', 'Mon', 'Wed', 'Fri'];
-  return days.map((day, i) => `<text y="${14 * i + 21}" x="-10">${day}</text>`).join('');
-}
-
-function generateCells(contributions, cellSize, cellPadding) {
-  let cellsSvg = '';
-  let x = 0;
+  let y = 0;
 
   contributions.weeks.forEach(week => {
-    let y = 0;
     week.contributionDays.forEach(day => {
       const count = day.contributionCount;
       let colorClass = 'contribution';
@@ -75,15 +67,19 @@ function generateCells(contributions, cellSize, cellPadding) {
       if (count > 10) colorClass = 'contribution-3';
       if (count > 20) colorClass = 'contribution-4';
 
-      cellsSvg += `<rect x="${x}" y="${y}" width="${cellSize}" height="${cellSize}" class="${colorClass}">
-        <title>${count} contributions on ${day.date}</title>
-      </rect>`;
+      svg += `<rect x="${x}" y="${y}" width="${cellSize}" height="${cellSize}" class="${colorClass}" />`;
 
       y += cellSize + cellPadding;
     });
 
+    y = 0;
     x += cellSize + cellPadding;
   });
 
-  return cellsSvg;
+  svg += '</svg>';
+
+  return { 
+    svg,
+    totalContributions: contributions.totalContributions
+  };
 }
